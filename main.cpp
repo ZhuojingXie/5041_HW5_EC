b6c6c850d7ac83d2316523e182978b45e101e716
ZhuojingXie <llxllxzj@gmail.com>
2021-03-10 16:19:34 -0800

    v2.0

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <iomanip>
 #include <vector>
-#include <algorithm>
+#include <sstream>
+#include <fstream>
+#include <string>
 #include "Task.h"
 #include "SJF.h"
 #include "NP.h"
@@ -10,7 +12,38 @@
 
 using namespace std;
 
+vector<vector<int>> filebuff (){
+    string filepath = "input.txt";
+    vector<vector<int>> res;
+
+
+
+    string buff;
+    ifstream inFile;
+    inFile.open(filepath);
+    if(!inFile)
+        throw runtime_error("File Not Found");
+
+    if(inFile){
+        while(getline(inFile,buff)){
+            istringstream iss(buff);
+            vector<int> process;
+            for(int n =0;n<4;n++){
+                int val;
+                iss>>val;
+                process.push_back(val);
+            }
+            res.push_back(process);
+        }
+    }
+
+    inFile.close();
+    return res;
+}
+
+
 int main() {
+    /*
     Task task1(0,0,5,3);
     Task task2(1,10,20,3);
     Task task3(2,10,10,5);
@@ -18,12 +51,6 @@ int main() {
     Task task5(4,20,30,1);
     Task task6(5,25,5,4);
 
-//    Task task1(0,0,5,3);
-//    Task task2(1,35,20,3);
-//    Task task3(2,10,10,5);
-//    Task task4(3,15,3,2);
-//    Task task5(4,20,35,1);
-//    Task task6(5,25,5,4);
 
     vector<Task> jobList;
     jobList.push_back(task1);
@@ -50,5 +77,30 @@ int main() {
     cout<<"====================PP===================\n\n";
     PP pp(jobList);
     pp.run();
+     */
+
+    vector<vector<int>> list = filebuff();
+    vector<Task> jobList;
+    for(auto process:list){
+        Task task(process[0],process[1],process[2],process[3]);
+        jobList.push_back(task);
+    }
+
+    cout<<"====================SFJ===================\n\n";
+    SJF sjf(jobList);
+    sjf.run();
+
+    cout<<"====================NP===================\n\n";
+    NP np(jobList);
+    np.run();
+
+    cout<<"====================SRTF===================\n\n";
+    SRTF srtf(jobList);
+    srtf.run();
+
+    cout<<"====================PP===================\n\n";
+    PP pp(jobList);
+    pp.run();
+
     return 0;
 }
